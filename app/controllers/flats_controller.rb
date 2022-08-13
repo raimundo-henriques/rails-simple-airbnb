dc6b660c6035d8b0bbe7e67704ed22026f0acfd5
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-13 13:06:53 +0200

    TA prep: flats#show partially done (no internet access, no access to bootstrap's documentation, nor to the challenge's page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,5 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show]
   def index
     @flats = Flat.all
   end
@@ -16,9 +17,16 @@ class FlatsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
