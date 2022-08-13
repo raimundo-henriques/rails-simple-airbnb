2563a128069ad354eccc0829c0f40822ffeeb812
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-13 13:27:06 +0200

    TA prep: flats#edit and #update

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show]
+  before_action :set_flat, only: [:show, :edit, :update]
   def index
     @flats = Flat.all
   end
@@ -18,6 +18,14 @@ class FlatsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
   end
 
   private
