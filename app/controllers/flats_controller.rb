bce6ef697410c33f8337de0246893a2f1f7be4b8
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-13 13:33:06 +0200

    TA prep: flats#destroy

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show, :edit, :update]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
   def index
     @flats = Flat.all
   end
@@ -26,6 +26,11 @@ class FlatsController < ApplicationController
   def update
     @flat.update(flat_params)
     redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
