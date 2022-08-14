3c3304075e39fca04e8ac0654bd8670165590cac
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-14 10:19:07 +0100

    TA prep: added pictures to flats (optional 1)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_084341) do
+ActiveRecord::Schema.define(version: 2022_08_14_090107) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_08_12_084341) do
     t.integer "number_of_guests"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture"
   end
 
 end
